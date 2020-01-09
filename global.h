4b4e924c6457f824b6ac52da64b8a308fe286cec
ruclty <2015200234@ruc.edu.cn>
2020-01-09 21:36:22 +0800

    update rpc

--- global.h
+++ global.h
@@ -155,13 +155,32 @@ struct predicateT{
 	}
 };     // TAB 型谓词 e.g Table1.attribute = Table2.attribute
 
-vector<string> split(string str, string delim) {
-    regex re{ delim };
-    return vector<std::string> {
-        sregex_token_iterator(str.begin(), str.end(), re, -1),
-            sregex_token_iterator()
-    };
-}     // 分词
+//#vector<string> split(string str, string delim) {
+//#    regex re{ delim };
+//#    return vector<std::string> {
+//#        sregex_token_iterator(str.begin(), str.end(), re, -1),
+//#            sregex_token_iterator()
+//#    };
+//#}     // 分词
+vector<string> split(const string& str, const string& delim) {  
+    vector<string> res;  
+    if("" == str) return res;  
+    //先将要切割的字符串从string类型转换为char*类型  
+    char * strs = new char[str.length() + 1] ; //不要忘了  
+    strcpy(strs, str.c_str());   
+ 
+    char * d = new char[delim.length() + 1];  
+    strcpy(d, delim.c_str());  
+     
+    char *p = strtok(strs, d);  
+    while(p) {  
+        string s = p; //分割得到的字符串转换为string类型  
+        res.push_back(s); //存入结果数组  
+        p = strtok(NULL, d);  
+    }  
+ 
+    return res;  
+}
 
 int c_in_str(string str, const char c){
     int count = 0;
--- rpc/rpc_receive.cpp
+++ rpc/rpc_receive.cpp
@@ -2,17 +2,23 @@
 // Created by dyj on 2019/12/8.
 //
 #include "rpc_receive.h"
-#include <rpc/client.h>
-//#include "global.h"
+//#include "../querytree/querytree.h"
+#include "../excute/site_excution.cpp"
+#include "../global.h"
 #include  <map>
-
+#include <pthread.h>
 using namespace std;
-// rpc_receive::rpc_receive(int site_id){
-//     this->site_id = site_id;
-// //    this->site_exc = site_excution(site_id);
-// }
-//rpc_receive::~rpc_receive(){}
-// vector<string> split(string& src,const string& separator){
+
+#define BUFFER_SIZE 1024
+//int site_id = 1;
+
+rpc_receive::rpc_receive(int site_id){
+    this->site_id = site_id;
+    this->site_exc = site_excution(site_id);
+}
+rpc_receive::~rpc_receive(){}
+
+//vector<string> split(string& src,const string& separator){
 //     vector<string> dest;
 //     if(src == "")
 //         return dest;
@@ -30,9 +36,9 @@ using namespace std;
 //             start =index+separator.size();
 //             index = str.find(separator,start);
 //             if (start == string::npos) break;
-//         }
+//        }
 //     }while(index != string::npos);
-
+//
 //     substring = str.substr(start);
 //     dest.push_back(substring);
 //     return dest;
@@ -55,7 +61,70 @@ OPERATORTYPE mapStringtoEnum(string op)
     return l_it->second;
 }
 
-bool ReceivePlan(string plans,site_excution* site_exc)
+struct args{
+    void * sock_fd;
+    rpc_receive * rpc_rec;
+};
+
+void* Data_handle(void * arg) {
+    args *pstru;
+    pstru = (struct args*) arg;
+    void * sock_fd = pstru->sock_fd;
+    rpc_receive * rpc_rec = pstru->rpc_rec;
+
+    int new_server_socket = *((int *) sock_fd);
+    char buffer[BUFFER_SIZE];
+    bzero(buffer, sizeof(buffer));
+    int length = recv(new_server_socket, buffer, BUFFER_SIZE, 0);
+    if (length < 0) {
+        printf("Server Recieve Operation Failed!\n");
+    }
+/*    char operation[OPERATION_NAME_MAX_SIZE + 1];
+    bzero(operation, sizeof(operation));
+    strncpy(operation, buffer,
+            strlen(buffer) > OPERATION_NAME_MAX_SIZE ? OPERATION_NAME_MAX_SIZE : strlen(buffer));
+    cout << "Data_handle operation\t" << operation << endl;*/
+    if(buffer[0]=='0')
+    {
+        //string plans;
+        //strncpy(plans,operation+1,OPERATION_NAME_MAX_SIZE+1);
+        string temp = buffer;
+        string plans(temp,1,BUFFER_SIZE-1);
+        //string plans = operation;
+        cout << "Data_handle plans\t" << plans << endl;
+        rpc_rec->ReceivePlan(plans);
+    }
+    else if(buffer[0]=='1')
+    {
+        string temp = buffer;
+        string sfrag_id(temp,1,1);
+        int frag_id=atoi(sfrag_id.c_str());
+        string frag_content(temp,3,BUFFER_SIZE-3);
+        vector<string> frag_content_origin_table_name;
+        frag_content_origin_table_name=split(frag_content,"#");
+        frag_content=frag_content_origin_table_name[0];
+        string origin_table_name= frag_content_origin_table_name[1];
+        //cout << "Data_handle\t" << plans << endl;
+        rpc_rec->ReceiveTable(frag_id,frag_content,origin_table_name);
+    }
+    else if(buffer[0]=='2')
+    {
+        string temp = buffer;
+        string sfrag_id(temp,1,1);
+        int frag_id=atoi(sfrag_id.c_str());
+        string frag_content(temp,3,BUFFER_SIZE-3);
+        vector<string> frag_content_origin_table_name;
+        frag_content_origin_table_name=split(frag_content,"#");
+        frag_content=frag_content_origin_table_name[0];
+        string origin_table_name= frag_content_origin_table_name[1];
+        
+        rpc_rec->ReceiveResultTable(frag_id,frag_content,origin_table_name);
+        //cout << "Data_handle\t" << plans << endl;
+        //rpc_rec.excute_result_table(frag_id,frag_content);
+    }
+}
+
+void rpc_receive::ReceivePlan(string plans)
 {
     vector<Operator> results;
     std::cout <<  "have received plan\t" << plans  << endl;
@@ -68,12 +137,13 @@ bool ReceivePlan(string plans,site_excution* site_exc)
         {
             std::cout <<  "vecplans\t" << vecplans[j]  << endl;
         }
-
         Operator plan;
         plan.content = vecplans[0];
         OPERATORTYPE ope=mapStringtoEnum(vecplans[1]);
         plan.ope = ope;
+        std::cout <<  "plan.ope\t" << plan.ope  << endl;
         plan.result_frag_id = std::stoi(vecplans[2]);
+        std::cout <<  "plan.result_frag_id\t" << plan.result_frag_id  << endl;
         plan.target_site_id = std::stoi(vecplans[3]);
         plan.is_end = std::stoi(vecplans[4]);
         
@@ -81,74 +151,97 @@ bool ReceivePlan(string plans,site_excution* site_exc)
             plan.table_names.push_back(vecplans[j]);
 
         results.push_back(plan);
+        vector<Operator>::iterator it = results.begin();
+        //std::cout << (*it).table_names <<endl;
     }
-
-    site_exc->sql_queue = results;
-    site_exc->excute_results(results);
-    return true;
+    //this->received_plan = results;
+    this->site_exc.sql_queue = results;
+    vector<Operator> to_do = this->site_exc.check_plan();
+    this->site_exc.excute_results(to_do);
+    //std::cout<< "rpc_receive::received_plan[0].table_names\t" << this->received_plan[0].table_names << endl;
+    //return true;
 }
 
 
-bool ReceiveTable(int frag_id, string frag_content, site_excution* site_exc)
+void rpc_receive::ReceiveTable(int frag_id, string frag_content,string origin_table_name)
 {
-    std::cout <<  "have received table\t" << frag_id  << endl;
-    std::cout <<  "have received table\t" << frag_content  << endl;
-    string table_name = site_exc->mysql.gdd.get_frag_info(frag_id).table_name;
-    excute_rec_table(table_name, frag_content,site_exc);
-    return true;
+    std::cout <<  "have received frag_id \t" << frag_id  << endl;
+    std::cout <<  "have received frag_content\t" << frag_content  << endl;
+    //this->received_frag_id = frag_id;
+    //this->received_frag_content = frag_content;
+    vector<Operator> to_do = site_exc.recieve_and_check(frag_id, frag_content, origin_table_name);
+    site_exc.excute_results(to_do);
+//    string table_name = this->site_exc.mysql.gdd.get_frag_info(frag_id).table_name;
+//    excute_rec_table(table_name, frag_content);
+    //return true;
 }
-
-bool ReceiveResult(int frag_id, string frag_content, site_excution* site_exc)
+void rpc_receive::ReceiveResultTable(int frag_id, string frag_content, string origin_table_name)
 {
-    std::cout <<  "have received table\t" << frag_id  << endl;
-    std::cout <<  "have received table\t" << frag_content  << endl;
-    string table_name = site_exc->mysql.gdd.get_frag_info(frag_id).table_name;
-    excute_result_table(table_name,frag_content,site_exc);
-    return true;
+    std::cout <<  "have received frag_id \t" << frag_id  << endl;
+    std::cout <<  "have received frag_content\t" << frag_content  << endl;
+ //   this->received_frag_id = frag_id;
+ //   this->received_frag_content = frag_content;
+    this->site_exc.recieve_result_table(frag_id, frag_content, origin_table_name);
+    //this->site_exc.excute_results(can_do);
+//    string table_name = this->site_exc.mysql.gdd.get_frag_info(frag_id).table_name;
+//    excute_rec_table(table_name, frag_content);
+   // return true;
 }
+void startListening(int port, rpc_receive * rpc_rec){
+    // set socket's address information
+    // 声明并初始化一个服务器端的socket地址结构
+    // 设置一个socket地址结构server_addr,代表服务器internet的地址和端口
+	struct sockaddr_in client_addr;
+	socklen_t length = sizeof(client_addr);
 
-void startListening(int port){
-    std::cout << "StartListening" << std::endl;
-    std::cout << port << std::endl;
-    rpc::server srv(port);
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    server_addr.sin_port = htons(port);
 
-
-    srv.bind("ReceivePlan",&ReceivePlan);
-    srv.bind("ReceiveTable",&ReceiveTable);
-    //srv.bind("ReceivePlan",&ReceivePlan);
-
-
-    srv.suppress_exceptions(true);
-    std::cout << "srv.run()" << std::endl;
-    srv.run();
-}
-
-void excute_rec_table(string table_name, string table_content, site_excution* site_exc){
-    vector<Operator> excution_plans;
-    excution_plans = site_exc->recieve_and_check(table_name);
-
-    int frag_id = site_exc->mysql.gdd.from_name_find_frag_id(table_name);
-    site_exc->mysql.create_received_table(table_content,frag_id,table_name);
-    site_exc->table_queue.push_back(table_name);
-    site_exc->excute_results(excution_plans);
-}
-// vector<Operator> save_rec_plan(vector<Operator> plans){
-//     vector<Operator> results;
-//     for(int i=0; i < plans.size(); i++)
-//         results.push_back(plans[i]);  
-//     return results; 
-// }
-void excute_result_table(string table_name, string table_content,site_excution* site_exc){
-    int frag_id = site_exc->mysql.gdd.from_name_find_frag_id(table_name);
-    frag_info result_info = site_exc->mysql.gdd.get_frag_info(frag_id);
-    for(int i=0;i<result_info.attr_names.size();i++)
+    // create a stream socket
+    // 创建socket，若成功，返回socket描述符
+    // 创建用于internet的流协议(TCP)socket，用server_socket代表服务器向客户端提供服务的接口
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
     {
-        cout << result_info.attr_names[i];
-        if(i != result_info.attr_names.size()-1)
-            cout << ",";
-        else
-            cout << endl;
+        printf("Create Socket Failed!\n");
+        exit(1);
+    }
+    // 把socket和socket地址结构绑定
+    // 绑定socket和socket地址结构
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)))
+    {
+        printf("Server Bind Port: %d Failed!\n", port);
+        exit(1);
+    }
+    // server_socket用于监听
+    if (listen(server_socket, 20))
+    {
+        printf("Server Listen Failed!\n");
+        exit(1);
+    }
+    while(1)
+    {
+        printf("waiting for new connection...\n");
+        pthread_t thread_id;
+        int new_server_socket = accept(server_socket, (struct sockaddr*)&client_addr, &length);
+        if (new_server_socket < 0)
+        {
+            printf("Server Accept Failed!\n");
+            continue;
+        }
+        printf("A new connection occurs!\n");
+        struct args arg = args{(void *)(&new_server_socket), rpc_rec};
+        if (pthread_create(&thread_id,NULL,&Data_handle,(void *)(&arg)) == -1)  //创建一个线程
+        {
+            fprintf(stderr,"pthread_create error!\n");
+            break;                                  //结束循环
+        }
     }
 
-    cout << table_content << endl;
+    // 关闭监听用的socket
+    close(server_socket);
 }
+
